bf98c58047939a0c052921deae4b6642ef26bc9a
Alexander Spanos <alexisspa9@gmail.com>
2018-04-01 18:40:50 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,6 @@
-# class AddSeasonToShows < ActiveRecord::Migration[4.2]
-# def change
-#   add_column :shows, :season, :string
-#
-# end
-# end
+class AddSeasonToShows < ActiveRecord::Migration
+def change
+  add_column :shows, :season, :string
+
+end
+end
