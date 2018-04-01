a491c9e13617d137034992180b8ba4e63938b230
Alexander Spanos <alexisspa9@gmail.com>
2018-04-01 19:05:50 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
   def self.most_popular_show
-    id = highest_rating
-    Show.find_by(id: id)
+    Show.maximum(:rating)
+
   end
   def self.lowest_rating
     Show.minimum(:rating)
