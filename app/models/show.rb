ec9032346310e51885f55a71b618635a6efb0c6c
Alexander Spanos <alexisspa9@gmail.com>
2018-04-01 19:08:22 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end
   def self.least_popular_show
-    Show.minimum(:rating)
+    Show.all.order(rating: ).first
   end
   def self.ratings_sum
 Show.sum(:rating)
